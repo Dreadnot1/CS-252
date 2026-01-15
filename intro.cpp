3e6c500391d75c5e95bfaedd4252f98a05b9463f
Dreadnot1 <soulfight1@proton.me>
2026-01-15 12:03:18 -0500

    first commit

---
+++ intro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*Functions need a return type
+Lines terminate with a semicolon*/
+int main() {
+    /*Console output uses << operator
+    Strings use double quotes*/
+    cout << "Hello World\n";
+
+    /*Variables also need a return type and can be declared*/
+    int age;
+    cin >> age;
+    cout << age;
+
+    /*0 is standard for everything running properly*/
+    return 100;
+}
