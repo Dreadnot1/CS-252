0f2815c74e327a6db46c8682588692e81c50877e
Dreadnot1 <soulfight1@proton.me>
2026-01-20 14:36:40 -0500

    Reorganized some notes

--- Week 1/intro.cpp
+++ Week 1/intro.cpp
@@ -1,18 +1,32 @@
+//# is a preprocessing directive
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-/*Functions need a return type
-Lines terminate with a semicolon*/
+//Functions need a return type
 int main() {
+    //Strings use double quotes, characters single quotes
+    string intro = "Hello World";
+    char single = 'H';
+
     /*Console output uses << operator
-    Strings use double quotes*/
+    Console input uses >> operator
+    */
     cout << "Hello World\n";
 
-    /*Variables also need a return type and can be declared*/
+    //Variables also need a return type and can be declared
     int age;
     cin >> age;
     cout << age;
 
-    /*0 is standard for everything running properly*/
+    //0 is standard for everything running properly
     return 100;
-}
+} 
+
+/* Some helpful reminders moving forward: 
+Lines terminate with a semicolon
+Brackets are required for 2+ lines of code
+And finally, indentation is not required for the C++ compiler 
+(But do it anyway for readability)
+*/
