989ffe7adccca771c9d5703a1cd1458f4759794b
Dreadnot1 <soulfight1@proton.me>
2026-01-31 15:17:30 -0500

    Problem 5-1

--- Labs/Lab 5/lab-5.cpp
+++ Labs/Lab 5/lab-5.cpp
@@ -3,17 +3,39 @@ using namespace std;
 
 int main() {
     const int MIN = 1;
-    int count;
+    const int MAX = 4;
+    int choice, operand1, operand2;
 
-    cout << "Enter an integer greater than 1: ";
-    cin >> count;
-    while (count <= MIN) {
-        cout << "Not a valid number, please try again: ";
-        cin >> count;
+    //Menu options
+    cout << "Enter [1] for addition\n";
+    cout << "Enter [2] for subtraction\n";
+    cout << "Enter [3] for multiplication\n";
+    cout << "Enter [4] to quit\n";
+
+    //Input validation
+    cin >> choice;
+    while (choice < MIN || choice > MAX) {
+        cout << "Invalid choice, please try again: ";
+        cin >> choice;
     }
 
-    for (int i = 1; i <= count; i++) {
-        cout << i << endl;
+    if (choice == 4) {
+        cout << "Goodbye!";
+        exit(0);
+    }
+
+    //Getting input for calculations
+    cout << "Enter operand 1: ";
+    cin >> operand1;
+    cout << "Enter operand 2: ";
+    cin >> operand2;
+
+    if (choice == 1) {
+        cout << operand1 + operand2;
+    } else if (choice == 2) {
+        cout << operand1 - operand2;
+    } else {
+        cout << operand1 * operand2;
     }
 
     return 0;
