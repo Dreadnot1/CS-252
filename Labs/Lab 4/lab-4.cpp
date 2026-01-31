f8640ebae19c1959dabddbc97b1b9c5b49369bda
Dreadnot1 <soulfight1@proton.me>
2026-01-31 14:26:37 -0500

    Assignment part three

--- Labs/Lab 4/lab-4.cpp
+++ Labs/Lab 4/lab-4.cpp
@@ -5,12 +5,23 @@ using namespace std;
 int main() {
 // 1. Declare variables
     int n = 0;
+    const int MAX = 50;
     int user_count;
-    string compliment = "You're the best around, just like the Karate Kid!";
+    string compliment = "You've got all the makings of a varsity athlete!";
+
 // 2. Get input (if needed)
     cout << "How many times would you like a compliment? ";
+
+    //Input validation to bound user_count to a constant
     cin >> user_count;
-// 3. Process data
+    while (user_count > MAX) {
+        cout << "\t Error! Please enter an integer less than or equal to "
+        << MAX << " please: ";
+        
+        cin >> user_count;
+    }
+
+// 4. Output results
     for (int i = 0; i < 11; i++) {
         n += i;
         cout << i << "\t" << n << endl;
@@ -19,7 +30,6 @@ int main() {
     for (int i = 0; i < user_count; i++) {
         cout << compliment << endl;
     }
-    
-// 4. Output results
+
     return 0;
 }
