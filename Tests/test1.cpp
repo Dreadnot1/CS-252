3efc099c2de43c7477379fe814144fb1eb18c9c7
Dreadnot1 <soulfight1@proton.me>
2026-02-03 16:41:28 -0500

    Modulus and exponents

--- Tests/test1.cpp
+++ Tests/test1.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+const int COLUMNS = 3;
+
 int main() {
-    int age = 99;
-    int wt = 345;
-    int ht = 67;
-    cout << age << endl << wt << endl << ht << endl;
+    // Working with exponents
+    int x = 3, y = 4;
+    int result = pow(x, 2) + (x * y) + pow(y, 2);
+    cout << result << endl;
+
+    // Using modulus to make columns
+    int sum = 0, number = 12;
+    for (int i = 1; i <= number; i++) {
+        sum += i;
+        cout << sum << "\t";
+
+        if (i % COLUMNS == 0) {
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
