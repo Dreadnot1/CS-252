a33dddf42e3ce8f1fe2f264fa8f5d1a165d0a29f
Dreadnot1 <soulfight1@proton.me>
2026-02-03 20:46:38 -0500

    Do while loop added

--- Labs/Lab 5/lab-5.cpp
+++ Labs/Lab 5/lab-5.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 using namespace std;
 
+const int MIN = 1;
+const int MAX = 4;
+
 int main() {
-    const int MIN = 1;
-    const int MAX = 4;
     int choice, operand1, operand2;
 
-    //Menu options
-    cout << "Enter [1] for addition\n";
-    cout << "Enter [2] for subtraction\n";
-    cout << "Enter [3] for multiplication\n";
-    cout << "Enter [4] to quit\n";
+    do {
+        //Menu options
+        cout << "Enter [1] for addition\n";
+        cout << "Enter [2] for subtraction\n";
+        cout << "Enter [3] for multiplication\n";
+        cout << "Enter [4] to quit\n";
 
-    //Input validation
-    cin >> choice;
-    while (choice < MIN || choice > MAX) {
-        cout << "Invalid choice, please try again: ";
+        //Input validation
         cin >> choice;
-    }
+        while (choice < MIN || choice > MAX) {
+            cout << "Invalid choice, please try again: ";
+            cin >> choice;
+        }
 
-    if (choice == 4) {
-        cout << "Goodbye!";
-        exit(0);
-    }
+        if (choice != 4) {
+            //Getting input for calculations
+            cout << "Enter operand 1: ";
+            cin >> operand1;
+            cout << "Enter operand 2: ";
+            cin >> operand2;
+        }
 
-    //Getting input for calculations
-    cout << "Enter operand 1: ";
-    cin >> operand1;
-    cout << "Enter operand 2: ";
-    cin >> operand2;
+        if (choice == 1) {
+            cout << operand1 + operand2 << endl;
+        } else if (choice == 2) {
+            cout << operand1 - operand2 << endl;
+        } else if (choice == 3) {
+            cout << operand1 * operand2 << endl;
+        } else {
+            cout << "Goodbye!" << endl;
+        }
 
-    if (choice == 1) {
-        cout << operand1 + operand2;
-    } else if (choice == 2) {
-        cout << operand1 - operand2;
-    } else {
-        cout << operand1 * operand2;
-    }
+    } while (choice != 4);
 
     return 0;
 }
