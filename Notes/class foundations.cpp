7515d523303e65f7011418f7358b4e792c8e72d6
Dreadnot1 <soulfight1@proton.me>
2026-03-23 02:12:17 -0400

    Added info on single and multilevel inheritance

--- Notes/class foundations.cpp
+++ Notes/class foundations.cpp
@@ -6,7 +6,7 @@ They're analagous to "blueprints" for creating objects.
 Attributes (variables) and methods (functions) are referred to as class members.
 */
 
-//Class definition
+//Class definition (Base/Parent)
 class Goat {
     /*Public and private are both examples of access specifiers.
     By default, all members of a class are private.*/
@@ -31,7 +31,7 @@ class Goat {
             color = c;
         }
 
-        /*Having more than one constructor in the same class allows flexibility.
+        /*Having more than one constructor in the same class allows more flexibility.
         This is called Constructor Overloading.*/
         Goat() { //default constructor
             age = 0;
@@ -42,6 +42,22 @@ class Goat {
         void talk() {
             cout << "Hello! I'm a talking goat." << endl;
         }
+
+        //Friend functions aren't members of the class but can access private data
+        friend void showAge(Goat gt);
+};
+
+/*Sub-class that demonstrates the concept of single inheritance (Derived/Child)
+It inherits the attributes and methods from the Base class
+Inheritance advantages: code reusability, abstraction, class hierarchy, polymorphism
+Disadvantages: tight coupling, reduced flexibility, overhead of virtual functions*/
+class BabyGoat: public Goat {
+    public:
+        bool cries = true;
+};
+
+//Sub-class that demonstrates multilevel inheritence
+class UnbornGoat: public BabyGoat {
 };
 
 //Method definitions outside the class use a scope resolution (::) operator
@@ -67,17 +83,31 @@ void Goat::printGoat() {
     cout << "Its coat is " << color << " in color." << endl;
 }
 
+//Friend function definition
+void showAge(Goat gt) {
+    cout << "Here's that Age you asked for: " << gt.age << " years old." << endl;
+}
+
 int main()
 {
     //Creates Goat objects with different types of constructors
     Goat myGoat(10, "White"); //parameterized
     Goat newGoat; //default
+
+    //Creating a child object that inherits from Goat class
+    BabyGoat myBabyGoat;
     
     //Method calls
     myGoat.setAge(5);
     myGoat.setColor("Brown");
     myGoat.printGoat();
     newGoat.printGoat();
+    myBabyGoat.setAge(1);
+    myBabyGoat.printGoat();
 
+    //Using a friend function to quickly display the age
+    showAge(myGoat);
+
+    
     return 0;
 }
