afe4b35f236dc203b65cf61b9b0274d19ec2900f
Dreadnot1 <soulfight1@proton.me>
2026-04-08 12:08:21 -0400

    Menu system to manipulate a vector full of objects with random attributes

--- Labs/Lab 24/villain class menu.cpp
+++ Labs/Lab 24/villain class menu.cpp
@@ -63,20 +63,49 @@ public:
              << setw(HCW) << henchmenCount
              << setw(CW);
         if (hasCape == 0) {
-            cout << "Yes" << endl;
-        } else {
             cout << "No" << endl;
+        } else {
+            cout << "Yes" << endl;
         }
     }
     
     // printHeader() prints a table header to the console
-    // No parameters or return values
-    void printHeader() {
-        cout << endl << "*** Lair Villains ***" << endl;
-        cout << "Name" << setw(HW) << "Evil Rating "
-             << "Henchmen " << "Has Cape" << endl;
-        cout << "-------------------------------------------------" << endl;
+    // Parameters: an int value that determines the type of header
+    //          0: All Villains
+    //          1: Caped Villains
+    //          2: Capeless Villains
+    // Returns: none
+    void printHeader(int h) {
+        if (h == 1) {
+            cout << endl << "*** Caped Villains ***" << endl;
+            cout << "Name" << setw(HW) << "Evil Rating "
+                 << "Henchmen " << "Has Cape" << endl;
+            cout << "-------------------------------------------------" << endl;
+        } else if (h == 2) {
+            cout << endl << "*** Capeless Villains ***" << endl;
+            cout << "Name" << setw(HW) << "Evil Rating "
+                 << "Henchmen " << "Has Cape" << endl;
+            cout << "-------------------------------------------------" << endl;           
+        } else {
+            cout << endl << "*** Lair Villains ***" << endl;
+            cout << "Name" << setw(HW) << "Evil Rating "
+                 << "Henchmen " << "Has Cape" << endl;
+            cout << "-------------------------------------------------" << endl;
+        }
     }
+    
+    // hasCapeOn() is a getter for the hasCape attribute
+    bool hasCapeOn() {
+        return hasCape;
+    }
+    
+    // recruitHenchmen() is a setter for the henchmenCount attribute
+    void recruitHenchmen(int add) {
+        cout << "BEFORE: " << henchmenCount << endl;
+        henchmenCount += add;
+        cout << "AFTER: " << henchmenCount << endl;
+    }
+    
 };
 
 
@@ -111,35 +140,69 @@ int main() {
                      << lair.size() << endl << endl;
                 break;
             }
-            case 3:
+            case 3: // removes a Villain from the end of the vector
                 lair.pop_back();
                 cout << "Removed a Villain. Lair size: " << lair.size()
                      << endl << endl;
                 break;
-            case 4:
-                lair[0].printHeader(); //table header
+            case 4: // prints all of the Villains attributes to console
+                lair[0].printHeader(0);
                 for (Villain v : lair) {
                     v.print();
                 }
                 cout << endl;
                 break;
-            case 5:
-                cout << "Output capes" << endl;
+            case 5: { // outputs all the Villains with capes
+                int capeHeader = 1, capeCount = 0;
+                lair[0].printHeader(1);
+                bool capeOn;
+                for (Villain v : lair) {
+                    capeOn = v.hasCapeOn();
+                    if (capeOn == 1) {
+                        v.print();
+                        capeCount += 1;
+                    } else {
+                        continue;
+                    }
+                }
+                cout << endl << capeCount << " Villains with capes." 
+                     << endl << endl;
                 break;
-            case 6:
-                cout << "Output no capes" << endl;
+            }
+            case 6: { // outputs all the Villains without capes
+                int noCapeHeader = 2, noCapeCount = 0;
+                lair[0].printHeader(noCapeHeader);
+                bool capeOff;
+                for (Villain v : lair) {
+                    capeOff = v.hasCapeOn();
+                    if (capeOff == 0) {
+                        v.print();
+                        noCapeCount += 1;
+                    } else {
+                        continue;
+                    }
+                }
+                cout << endl << noCapeCount<< " Villains without capes." 
+                     << endl << endl;
                 break;
-            case 7:
-                cout << "Recruit" << endl;
+            }
+            case 7: { // recruits 3 henchmen for each Villain
+                int recruits = 3;
+                for (Villain v : lair) {
+                    v.recruitHenchmen(recruits);
+                }
+                cout << endl << "3 Henchmen recruited for each Villain." 
+                     << endl << endl;
                 break;
-            case 8:
+            }
+            case 8: // clears the Villain lair vector
                 lair.clear();
-                cout << "Lair closed!";
+                cout << endl << "Lair closed!" << endl << endl;
                 break;
         }
         printMenu();
         cin >> choice;
-    } while (choice != 9);
+    } while (choice != 9); //option 9 exits the loop and closes the program
 
     return 0;
 }
