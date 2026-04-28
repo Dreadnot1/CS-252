a3329a29599e5d7965fd8cb46b6494773b4bc958
Dreadnot1 <soulfight1@proton.me>
2026-04-28 16:18:04 -0400

    Template for structs

---
+++ Notes/structs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+// Similar to classes but can't use methods, all members are public by default
+// Good for efficient use of small, frequently used data
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+
+
+int main() {
+    Color lBlue = {173, 216, 230}
+    
+    return 0;
+}
